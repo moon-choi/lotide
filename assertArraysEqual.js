82826b4e170dad22e273a7bb7b7e0979d26fa449
Moon Choi <choimy1991@gmail.com>
2022-02-08 21:18:43 -0800

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const assertArraysEqual = function(arr1, arr2){
+    if (eqArrays()) { // if function returns true
+        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  
+}
+
+
+const eqArrays = function(arr1, arr2){
+	// Check if the arrays are the same length
+	if (arr1.length !== arr2.length) {
+        return false;
+    }    
+	// Check if all items exist and are in the same order
+	for (let i = 0; i < arr1.length; i++) {
+	    if (arr1[i] !== arr2[i]) {
+            return false;
+        } else {
+           return true;
+       }
+	}
+}    
+
