f4179d4923f4b80b54d29fa6c50556d94183f529
Moon Choi <choimy1991@gmail.com>
2022-02-12 10:46:03 -0800

    Implement tail

---
+++ tail.js
@@ -0,0 +1,23 @@
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const tail = function(array){
+// let shifted = [];   
+    if (array.length > 0) {
+        console.log(array.slice(1));
+        return array
+    } 
+    return [];      
+}
+
+// Test Case: Check the original array 
+tail(words); // no need to capture the return value since we are not checking it
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+assertEqual(words.length, 3); // original array should still have 3 elements!
