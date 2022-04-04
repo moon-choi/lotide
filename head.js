7ef9cca1ef900219768ad32fca3dfcdf858b8819
Moon Choi <choimy1991@gmail.com>
2022-04-04 14:23:16 -0700

    head.js

--- head.js
+++ head.js
@@ -1,28 +1,34 @@
-const head = function(array, target){
-    for(let i = 0; i < array.length; i++) {
-        if (array[i] === target) {
-            return array[i]
-        }
-        return array[i] // is this return correct? or unnecessary?
-    }
-}
-const result = head([5,6,7], 5) 
+const head = function (arr) {
+  return arr[0];
+};
 
-/* refactored
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-*/  
-  
+// const head = function (array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === target) {
+//       return array[i];
+//     }
+//     return array[i]; // is this return correct? or unnecessary?
+//   }
+// };
+// const result = head([5, 6, 7], 5);
+
+// refactored
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
 /* moved to test folder
 assertEqual(result, 5)
 */
 
 module.exports = head;
 
-console.log(head([1, 2, 3], 1))
-console.log(head(['5']))
+console.log(head([1, 2, 3], 1));
+console.log(head(["5"]));
