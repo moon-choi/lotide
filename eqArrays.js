b9b382531fb44eba928ae61932493802f297fb2b
Moon Choi <choimy1991@gmail.com>
2022-04-05 10:55:08 -0700

    apr

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,40 @@
-const eqArrays = function(arr1, arr2){
-	// Check if the arrays are the same length
-	if (arr1.length !== arr2.length) {
-        return false;
-    }    
-	// Check if all items exist and are in the same order
-	for (let i = 0; i < arr1.length; i++) {
-	    if (arr1[i] !== arr2[i]) {
-            return false;
-	    } 
-  }    
-  return true;
-}    
+//apr solution
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+      return false;
+    }
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+// feb solution
+// const eqArrays = function(arr1, arr2) {
+//   // Check if the arrays are the same length
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   // Check if all items exist and are in the same order
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
 /*
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
