ee405d67550dcaacf09567eaaebb78b0184a401b
Moon Choi <choimy1991@gmail.com>
2022-04-05 21:45:23 -0700

    apr

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,29 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(actual, expected){
-    if (eqArrays(actual, expected)) { // if function returns true
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    // if function returns true
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  // Check if the arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // Check if all items exist and are in the same order
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+      return true;
     }
-}
-/*
-const eqArrays = function(arr1, arr2){
-	// Check if the arrays are the same length
-	if (arr1.length !== arr2.length) {
-        return false;
-    }    
-	// Check if all items exist and are in the same order
-	for (let i = 0; i < arr1.length; i++) {
-	    if (arr1[i] !== arr2[i]) {
-            return false;
-        } else {
-           return true;
-       }
-	}
-}    
+  }
+};
 
 assertArraysEqual([1, 2, 3], [2, 3]);
-*/
 
 module.exports = assertArraysEqual;
