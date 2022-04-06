9ab5e3522b28dda923cf4bd79d05a52b969d15e2
Moon Choi <choimy1991@gmail.com>
2022-04-06 14:39:41 -0700

    apr

--- countLetters.js
+++ countLetters.js
@@ -1,25 +1,50 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
+
+
+// apr solution
+const string = "LHL";
+const obj = {};
+
+const countLetters = function(string) {
+  const newString = string.replace(/\s/g, ""); // only take away spaces at the end and at the beginning
+
+  for (let x of newString) {
+    let counter = 1;
+
+    if (!obj[x]) {
+      //checking if this is undefiend
+      // if (!obj.hasOwnProperty(x)) { //checking if the object has the property.
+      obj[x] = counter;
+    } else {
+      //you need an else statement.
+      obj[x] += 1;
+    }
+  }
+  return obj;
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual.L === expected.L) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-const countLetters = function (word) {
-  const count = {};
-  const trimWord = word.replace(/\s/g, '') // only take away spaces at the end and at the beginning
-  console.log(trimWord)
-  for (let i = 0; i < trimWord.length; i++) {
-    const letter = trimWord[i];
-    if (count[letter]) {
-      count[letter]++;
-    } else {
-      count[letter] = 1;
-    }
-  }
-  return count
-};
+assertEqual(countLetters(string), {L:2, H:1});
 
-console.log(countLetters("hello from the other side"));
+// feb solution
+// const countLetters = function (word) {
+//   const count = {};
+//   const trimWord = word.replace(/\s/g, '') // only take away spaces at the end and at the beginning
+//   console.log(trimWord)
+//   for (let i = 0; i < trimWord.length; i++) {
+//     const letter = trimWord[i];
+//     if (count[letter]) {
+//       count[letter]++;
+//     } else {
+//       count[letter] = 1;
+//     }
+//   }
+//   return count
+// };
