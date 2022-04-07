31bccc9d153360e425f6354a6ad4d58ef17d65c4
Moon Choi <choimy1991@gmail.com>
2022-04-07 14:14:50 -0700

    ANDY IS THE BEST

--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,51 @@
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1); // [c d]
+  const keys2 = Object.keys(object2); // [d c]
 
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  if (keys1.length === keys2.length) {
-    for (const key of keys1) {
-      if (!keys2.includes(key)) {
-        // if [a, b] does not include b
-        return false; //then two arrays don't match, so exit the loop.
+  if (keys1.length !== keys2.length) {
+    //2
+    return false;
+  }
+  // by failing the most wrong case, we don't have to indent everything below, also we don't need an else statement.
+
+  for (const key of keys1) {
+    // c, d
+    if (!keys2.includes(key)) {
+      //dc 에 c가 없는가 //dc에 d가 없는가
+      return false;
+    }
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        //[2,3] [2,3]
+        return false;
       }
-      //  “2"                  “2”
+    } else {
+      //anything that is not array
       if (object1[key] !== object2[key]) {
-        //if two arrays matched above, then we skip that if statement, and continue on this if statement.
-        return false; // if object1[a] values are not matching (1, 2), exit the loop.
+        //cd의 c 값과 dc의 c값이 다른가 //cd의 d값과 dc의 d값이 다른가 //this only works for primitives.
+        return false; //여기가 문제
       }
     }
-    return true; //when every if statement was true (when all for loop is finished)
-  } else { //when array lengths don't match, finish the function right away
-    return false;
+  }
+  return true;
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+      return false;
+    }
+  }
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -29,14 +56,10 @@ const result1 = eqObjects(ab, ba); // => true
 const abc = { a: "1", b: "2", c: "3" };
 const result2 = eqObjects(ab, abc); // => false
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const result3 = eqObjects(cd, dc); // => true 
 
 assertEqual(result1, true);
 assertEqual(result2, false);
+assertEqual(result3, true);
