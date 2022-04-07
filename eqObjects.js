a5a7192eff1f1621951c1d64f1c5cf497e4037c4
Moon Choi <choimy1991@gmail.com>
2022-04-06 22:47:24 -0700

    apr

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,26 @@
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
-const eqObjects = function (object1, object2) {
-    if (Object.keys(object1).length === Object.keys(object2).length) {
-      const obj2Keys = Object.keys(object2); //it’s an array including a, b
-      for (const obj1Key of Object.keys(object1)) { // **** obj1Key = a   Object.keys() = [a, b]
-        if (!obj2Keys.includes(obj1Key)) { // if [a, b] does not include b
-          return false; //then two arrays don't match, so exit the loop.
-        }
-               //  “2"                  “2”
-        if (object1[obj1Key] !== object2[obj1Key]) { 
-            //if two arrays matched above, then we skip that if statement, and continue on this if statement. 
-          return false; // if object1[a] values are not matching (1, 2), exit the loop. 
-        }
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2); //it’s an array including a, b
+    for (const key of keys1) {
+      // **** obj1Key = a   Object.keys() = [a, b]
+      if (!keys2.includes(key)) {
+        // if [a, b] does not include b
+        return false; //then two arrays don't match, so exit the loop.
       }
-      return true; //when if statement was true
-    } else {
-      return false;
+      //  “2"                  “2”
+      if (object1[key] !== object2[key]) {
+        //if two arrays matched above, then we skip that if statement, and continue on this if statement.
+        return false; // if object1[a] values are not matching (1, 2), exit the loop.
+      }
     }
+    return true; //when if statement was true
+  } else {
+    return false;
+  }
 };
 
 const ab = { a: "1", b: "2" };
@@ -27,15 +30,14 @@ const result1 = eqObjects(ab, ba); // => true
 const abc = { a: "1", b: "2", c: "3" };
 const result2 = eqObjects(ab, abc); // => false
 
-
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 assertEqual(result1, true);
 assertEqual(result2, false);
--- test/assertArraysEqual_test.js
+++ test/assertArraysEqual_test.js
@@ -1,3 +1,3 @@
-const assertArraysEqual = require("../assertArraysEqual")
+const assertArraysEqual = require("../assertArraysEqual");
 
 assertArraysEqual([1, 2, 3], [2, 3]);
