557878d9eb215638067bc59670ee3e4fa3c821a1
Moon Choi <choimy1991@gmail.com>
2022-04-07 17:08:25 -0700

    apr

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -12,29 +12,22 @@ const assertObjectsEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    const obj2Keys = Object.keys(object2); //it’s an array including a, b
-    for (const obj1Key of Object.keys(object1)) {
-      //obj1Key = a   Object.keys() = [a, b]
-      if (!obj2Keys.includes(obj1Key)) {
-        // if [a, b] does not include b
-        return false; //then two arrays don't match, so exit the loop.
-      }
-      //  “2"                  “2”
-      if (object1[obj1Key] !== object2[obj1Key]) {
-        //if two arrays matched above, then we skip that if statement, and continue on this if statement.
-        return false; // if object1[a] values are not matching (1, 2), exit the loop.
-      }
-    }
-    return true; //when if statement was true
-  } else {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
     return false;
   }
+
+  for (const key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-
 const abc = { a: "1", b: "2", c: "3" };
 
 assertObjectsEqual(ab, ba);
