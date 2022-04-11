ccd48c3e3ddc1007560d66328fe5ce8a8a82ed9a
Moon Choi <choimy1991@gmail.com>
2022-04-11 16:47:04 -0700

    apr refactor

--- test/head_test.js
+++ test/head_test.js
@@ -1,23 +1,17 @@
-/*
-refactored
-
+const assert = require('chai').assert;
 const head = require('../head');
 const assertEqual = require('../assertEqual');
-const result = head([5,6,7], 5)
 
-assertEqual(result, 5)
-*/
+// describe("#middle", () => {
+//   it("returns 1 for [1, 2, 3]", () => {
+//     assert.strictEqual(head([1, 2, 3]), 1);
+//   });
 
-const assert = require('chai').assert;
-const head   = require('../head');
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '5');
+//   });
+// });
 
-describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
-  });
-
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
-  });
-});
-
+head([1, 2, 3], 1);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/tail_test.js
+++ test/tail_test.js
@@ -2,5 +2,5 @@ const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words)
+tail(words);
 assertEqual(words.length, 3); // original array should still have 3 elements!
